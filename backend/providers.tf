32653c9aa1eafaee038db0180fe44413ab388275
nimbleclick <christian.dziachan@gmail.com>
2024-07-10 12:37:02 -0700

    feature add: implemented OAC in cloudfront distribution to restrict access to domain s3 bucket

--- backend/providers.tf
+++ backend/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.4.0"
+      version = "5.57.0"
     }
     archive = {
       source  = "hashicorp/archive"
